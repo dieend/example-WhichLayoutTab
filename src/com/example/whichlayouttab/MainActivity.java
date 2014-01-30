0e17fa287b85171f2a9a4bcb43f3b97849accde7
dieend <mail.dieend@gmail.com>
2014-01-30 20:12:16 +0700

    version a and version b in one app

--- src/com/example/whichlayouttab/MainActivity.java
+++ src/com/example/whichlayouttab/MainActivity.java
@@ -4,12 +4,16 @@ import java.util.Locale;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -39,9 +43,6 @@ public class MainActivity extends FragmentActivity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Set up the action bar.
-		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
@@ -52,26 +53,51 @@ public class MainActivity extends FragmentActivity implements
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
 
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						actionBar.setSelectedNavigationItem(position);
-					}
-				});
+		switch(Splitter.whichVersioN()) {
+			case 0:
+				final ActionBar actionBar = getActionBar();
+				// Set up the ViewPager with the sections adapter.
+				actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+				// For each of the sections in the app, add a tab to the action bar.
+				for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+					// Create a tab with text corresponding to the page title defined by
+					// the adapter. Also specify this Activity object, which implements
+					// the TabListener interface, as the callback (listener) for when
+					// this tab is selected.
+					actionBar.addTab(actionBar.newTab()
+							.setText(mSectionsPagerAdapter.getPageTitle(i))
+							.setTabListener(this));
+				}
+				// When swiping between different sections, select the corresponding
+				// tab. We can also use ActionBar.Tab#select() to do this if we have
+				// a reference to the Tab.
+				mViewPager
+						.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+							@Override
+							public void onPageSelected(int position) {
+								actionBar.setSelectedNavigationItem(position);
+							}
+						});
+				break;
+			case 1:
+				PagerTitleStrip strip = new PagerTitleStrip(this);
+				ViewPager.LayoutParams params = new ViewPager.LayoutParams();
+				params.width = ViewPager.LayoutParams.MATCH_PARENT;
+				params.height = ViewPager.LayoutParams.WRAP_CONTENT;
+				params.gravity = Gravity.TOP;
+				strip.setTextColor(Color.parseColor("#ffffff"));;
+				strip.setBackgroundColor(Color.parseColor("#33b5e5"));
+				float scale = getResources().getDisplayMetrics().density;
+				int dpAsPixels = (int) (4*scale + 0.5f);
+				strip.setPadding(0, dpAsPixels, 0, dpAsPixels);
+				mViewPager.addView(strip, 0, params);
+				mSectionsPagerAdapter = new SectionsPagerAdapter(
+						getSupportFragmentManager());
+				// Set up the ViewPager with the sections adapter.
 
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter. Also specify this Activity object, which implements
-			// the TabListener interface, as the callback (listener) for when
-			// this tab is selected.
-			actionBar.addTab(actionBar.newTab()
-					.setText(mSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
+				mViewPager.setAdapter(mSectionsPagerAdapter);
+
+				break;
 		}
 	}
 
@@ -166,6 +192,7 @@ public class MainActivity extends FragmentActivity implements
 					.findViewById(R.id.section_label);
 			dummyTextView.setText(Integer.toString(getArguments().getInt(
 					ARG_SECTION_NUMBER)));
+			Log.d("ADIN",""+container.getChildCount());
 			return rootView;
 		}
 	}
--- src/com/example/whichlayouttab/Splitter.java
+++ src/com/example/whichlayouttab/Splitter.java
@@ -2,6 +2,6 @@ package com.example.whichlayouttab;
 
 public class Splitter {
 	public static int whichVersioN() {
-		return 0;
+		return 1;
 	}
 }
